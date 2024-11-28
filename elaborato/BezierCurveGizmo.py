b190ecbb74b7182bc83cf9fc6721bf0b68913815
Itina99 <mattetina@outlook.it>
2024-11-28 17:21:31 +0100

    minor changes

--- elaborato/BezierCurveGizmo.py
+++ elaborato/BezierCurveGizmo.py
@@ -114,43 +114,9 @@ class CurveEditor:
         self.curve = self.construct_curve()  # Reconstruct the curve
         self.plot()  # Redraw the plot
 
-    
-
-
-""" def create_basis( degree = 2):
-        knot_array = np.zeros(degree + 1)
-        knot_array = np.append(knot_array, np.ones(degree + 1))
-        kv = gs.nurbs.gsKnotVector(np.array(knot_array), degree)
-        basis = gs.nurbs.gsBSplineBasis(kv)
-        return basis
-
-#construct curve
-def construct_curve():
-    pts = select_points()
-    basis = create_basis(len(pts) - 1)
-    coefs = np.zeros((len(pts), 2))
-    coefs[:, 0] = np.array([pt[0] for pt in pts])
-    coefs[:, 1] = np.array([pt[1] for pt in pts])
-
-    return gs.nurbs.gsBSpline(basis, coefs)
-
-
-def plot_curve(curve):
-    N = 100
-    x = np.linspace(0, 1, N)
-    x = np.matrix(np.meshgrid(x))
-    y = curve.eval(x)
-
-    plt.plot(y[0, :], y[1, :], label='Bezier Curve')
-    plt.scatter(curve.coefs()[:, 0], curve.coefs()[:, 1], color='red', label='Control Points')
-    plt.plot(curve.coefs()[:, 0], curve.coefs()[:, 1], linestyle='--', color='gray', label='Control Polygon')
-    plt.xlabel(r'$x$')
-    plt.ylabel(r'$y$')
-    plt.legend()
-    plt.show() """
 
 if __name__ == '__main__':
-    # Select points interactively
+    # Select points interactivel
     control_points, degree = select_points()
 
     # Initialize the editor
