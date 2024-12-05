7974caad7e919fb8fc274e9de8c6cdaf803f8313
Niccolò Menghini <niccolo.menghini@gmail.com>
2024-12-06 00:24:46 +0100

    Added curvature to curve editor class, added dynamic curvature in editor. Commented code to be possibly implemented in a futire instance (ignore for now)

--- elaborato/BezierCurveGizmo.py
+++ elaborato/BezierCurveGizmo.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pygismo as gs
 from matplotlib.widgets import Slider
+import math
 
 def select_points():
     plt.figure()
@@ -24,8 +25,9 @@ class CurveEditor:
         self.coefs = np.array(coefs)  # Control points
         self.degree = degree          # Degree of the curve
         self.curve = self.construct_curve()  # Initial curve
+        self.curvature_ = self.curvature()
         self.dragging_point = None
-        self.fig, self.ax = plt.subplots()
+        self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2)
         self.plot()  # Plot the curve and control points
         self.cid_press = self.fig.canvas.mpl_connect('button_press_event', self.on_press)
         self.cid_release = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
@@ -47,37 +49,44 @@ class CurveEditor:
         y_min, y_max = self.coefs[:, 1].min(), self.coefs[:, 1].max()
 
         # Set limits with a margin
-        self.ax.set_xlim(x_min - margin, x_max + margin)
-        self.ax.set_ylim(y_min - margin, y_max + margin)
+        self.ax1.set_xlim(x_min - margin, x_max + margin)
+        self.ax1.set_ylim(y_min - margin, y_max + margin)
 
     def plot(self):
         """Plot the curve, control points, and control polygon."""
-        self.ax.clear()
+        self.ax1.clear()
+        self.ax2.clear()
 
         # Restore fixed axes limits
         self.set_axes_limits()
 
         # Evaluate the curve
         N = 100
-        x = np.linspace(0, 1, N)
-        x = np.matrix(np.meshgrid(x))
+        x_space = np.linspace(0, 1, N)
+        x = np.matrix(np.meshgrid(x_space))
         y = self.curve.eval(x)
 
         # Plot the curve
-        self.ax.plot(y[0, :], y[1, :], label='Bezier Curve')
+        self.ax1.plot(y[0, :], y[1, :], label='Bezier Curve')
+        print(self.curvature_)
+        self.ax2.plot(x_space, self.curvature_, label='Curvature')
 
         # Plot control points and polygon
-        self.control_points, = self.ax.plot(self.coefs[:, 0], self.coefs[:, 1], 'ro', label='Control Points', picker=5)
-        self.control_polygon, = self.ax.plot(self.coefs[:, 0], self.coefs[:, 1], '--', color='gray', label='Control Polygon')
+        self.control_points, = self.ax1.plot(self.coefs[:, 0], self.coefs[:, 1], 'ro', label='Control Points', picker=5)
+        self.control_polygon, = self.ax1.plot(self.coefs[:, 0], self.coefs[:, 1], '--', color='gray', label='Control Polygon')
 
-        self.ax.legend()
-        self.ax.set_xlabel(r'$x$')
-        self.ax.set_ylabel(r'$y$')
+        self.ax1.legend()
+        self.ax1.set_xlabel(r'$x$')
+        self.ax1.set_ylabel(r'$y$')
+
+        self.ax2.legend()
+        self.ax2.set_xlabel(r'$x$')
+        self.ax2.set_ylabel(r'$y$')
 
         plt.draw()
 
     def on_press(self, event):
-        if event.inaxes != self.ax:
+        if event.inaxes != self.ax1:
             return
 
         # Initialize a variable to store the closest point's index and distance
@@ -98,11 +107,12 @@ class CurveEditor:
         self.dragging_point = None  # Clear the dragging state
 
     def on_motion(self, event):
-        if event.inaxes != self.ax or self.dragging_point is None:
+        if event.inaxes != self.ax1 or self.dragging_point is None:
             return
         # Update the position of the dragged point
         self.coefs[self.dragging_point] = [event.xdata, event.ydata]
         self.curve = self.construct_curve()  # Reconstruct the curve
+        self.curvature_ = self.curvature()
         self.plot()  # Redraw the plot
 
     def linear_precision(self):
@@ -151,6 +161,16 @@ class CurveEditor:
             ax.legend()
         plt.show()
 
+    def curvature(self):
+        N = 100
+        x = np.linspace(0, 1, N)
+        x = np.matrix(np.meshgrid(x))
+
+        a = self.curve.deriv(x)
+        b = self.curve.deriv2(x)
+
+        return np.linalg.norm(np.cross(a, b, axis=0), axis=0)/np.linalg.norm(a, axis=0)**3
+
 def bezier_3d(deg, curv=False):
     """
         Permette di disegnare una curva di Bezier nello spazio
@@ -198,15 +218,38 @@ def curvature(curve, param_space):
     return curvature
 
 """
-Funzione d'emergenza per il calcolo della derivata prima; equivalente a curve.deriv
-def st_derivative(points, param):
-    knot_array = np.zeros(2)
-    knot_array = np.append(knot_array, np.ones(2))
-    kv = gs.nurbs.gsKnotVector(np.array(knot_array), 1)
-    basis = gs.nurbs.gsBSplineBasis(kv)
+In cantiere, una funzione per calcolare la torsione, necessita di derivate terze
+def torsion(curve, param_sapce):
 
-    print(np.array(points[0+1, :] - points[0, :]))
+    a = curve.deriv()
+    b = curve.deriv2()
+"""    
 
+"""
+def delta(ctl_points, deg, idx):
+    if deg == 0:
+        return ctl_points[idx]
+    else:
+        return delta(ctl_points, deg-1, idx+1) - delta(ctl_points, deg -1, idx)
+"""
+
+"""
+Funzione d'emergenza per il calcolo della derivata; equivalente a curve.deriv
+def ith_derivative(pts, curve, i, param_range):
+    basis = curve.basis()
+    degree = curve.degree()
+
+    sum = 0
+    for j in range(degree - i + 1):
+        sum += np.matmul(delta(pts, i, j).reshape(2, 1), basis.evalSingle(j, param_range))
+    return (math.factorial(degree)/math.factorial(degree-i)) * sum 
+"""
+
+"""
+# Funzione d'emergenza per il calcolo della derivata prima; equivalente a curve.deriv
+def st_derivative(points, curve, param):
+    basis = curve.basis()
+    
     sum = 0
     for i in range(2):
         sum += np.matmul((points[i+1, :] - points[i, :]).reshape(2, 1), basis.evalSingle(i, param))
@@ -214,13 +257,31 @@ def st_derivative(points, param):
 """
 
 if __name__ == '__main__':
+    """# Creo la base di bernstein corrispondente al grado in input
+    knot_array = np.zeros(2 + 1)
+    knot_array = np.append(knot_array, np.ones(2+ 1))
+    kv = gs.nurbs.gsKnotVector(np.array(knot_array), 2)
+    basis = gs.nurbs.gsBSplineBasis(kv)
     
-    """
-    #Select points interactivel
+    # Prendo dei punti casuali nello spazio 3d
+    x_points = np.random.uniform(0, 10, 2 + 1)
+    y_points = np.random.uniform(0, 10, 2 + 1)
+    points = np.vstack((x_points, y_points)).T
+
+    curve = gs.nurbs.gsBSpline(basis, points)
+    N = 100
+    x_space = np.linspace(0, 1, N)
+    x = np.matrix(np.meshgrid(x_space))
+    
+    print(ith_derivative(points, curve, 1, x))
+    print(st_derivative(points, curve, x))
+    print(curve.deriv(x))"""
+
+    #Select points interactively
     control_points, degree = select_points()
 
     # Initialize the editor
     editor = CurveEditor(control_points, degree)
-    editor.linear_precision()"""
+    editor.linear_precision()
 
-    bezier_3d(5, curv=True)
+    # bezier_3d(5, curv=True)
