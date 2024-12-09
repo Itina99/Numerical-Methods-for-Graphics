bab220b60bbdb4c4d92791ee1259e876e4534cea
Itina99 <mattetina@outlook.it>
2024-12-09 17:35:26 +0100

    fixed problems with basis

--- elaborato/SplineBaseGismo.py
+++ elaborato/SplineBaseGismo.py
@@ -46,10 +46,10 @@ def compute_basis_evals(basis, N=100):
     return x_vals, evals
 
 # Interactive plot function
-def plot():
+def plot_degree_functions_sliders():
     # Initial setup
     fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.4)  # Leave more space for the sliders
+    plt.subplots_adjust(bottom=0.25)  # Leave more space for the sliders
 
     # Default values
     degree = 2
@@ -62,49 +62,24 @@ def plot():
     for i in range(0, basis.size()):
         line, = ax.plot(x_vals, evals[i, :], label=f'Basis function {i+1}')
         lines.append(line)
-    
-    # Plot the knot vector
-    knot_lines = ax.vlines(knot_vector, ymin=0, ymax=1, colors='gray', linestyles='dashed', label='Knots')
 
     ax.set_title('B-spline basis functions')
     ax.set_xlabel(r'$\xi$')
     ax.set_ylabel(r'$\varphi$')
 
     # Add sliders
-    ax_slider_functions = plt.axes([0.25, 0.25, 0.65, 0.03])
+    ax_slider_functions = plt.axes([0.25, 0.15, 0.65, 0.03])
     sliderfunctions = Slider(ax_slider_functions, 'Num basis functions', 1, 10, valinit=num_basis_functions, valstep=1)
     
-    ax_slider_degree = plt.axes([0.25, 0.2, 0.65, 0.03])
+    ax_slider_degree = plt.axes([0.25, 0.1, 0.65, 0.03])
     sliderdegree = Slider(ax_slider_degree, 'Degree', 1, 10, valinit=degree, valstep=1)
-
-    # Custom knot sliders
-    inner_knots = len(knot_vector) - 2 * (degree + 1)
-    custom_knot_sliders = []
-    custom_knot_axes = []
-
-    def create_custom_knots_sliders():
-        """Create the custom knot sliders based on the current knot vector"""
-        for ax in custom_knot_axes:
-            ax.clear()  # Remove previous sliders and axes
-        custom_knot_sliders.clear()  # Clear previous sliders
-
-        for i in range(inner_knots):
-            ax_slider = plt.axes([0.25, 0.15 - i * 0.05, 0.65, 0.03], facecolor='lightgoldenrodyellow')
-            slider = Slider(ax_slider, f'Knot {i + 1}', 0.01, 0.99, valinit=knot_vector[degree + 1 + i])
-            custom_knot_sliders.append(slider)
-            custom_knot_axes.append(ax_slider)
-
-    # Initial custom knot sliders
-    create_custom_knots_sliders()
 
     def update():
         # Update basis, knot vector, and evaluations
         num_basis_functions = int(sliderfunctions.val)
         degree = int(sliderdegree.val)
         
-        # Update custom knots
-        custom_knots = np.array([slider.val for slider in custom_knot_sliders])
-        basis, knot_vector = create_basis(degree, num_basis_functions, custom_knots=custom_knots)
+        basis, knot_vector = create_basis(degree, num_basis_functions)
         x_vals, evals = compute_basis_evals(basis)
 
         # Adjust number of lines dynamically
@@ -119,23 +94,86 @@ def plot():
         for i, line in enumerate(lines):
             line.set_data(x_vals, evals[i, :])
 
-        # Update knot markers
-        for kline in ax.collections:
-            kline.remove()  # Clear old knots
-        ax.vlines(knot_vector, ymin=0, ymax=1, colors='gray', linestyles='dashed')
-
-        # Recreate custom knot sliders
-        create_custom_knots_sliders()
-
         fig.canvas.draw_idle()
 
     # Connect sliders to update function
     sliderfunctions.on_changed(lambda val: update())
     sliderdegree.on_changed(lambda val: update())
-    for slider in custom_knot_sliders:
-        slider.on_changed(lambda val: update())
 
     plt.show()
+
+
+def plot_with_knot_sliders():
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.4)  # Leave more space for the sliders
+
+    # User input for initial degree and number of basis functions
+    degree = int(input("Degree: "))
+    num_basis_functions = int(input("Number of basis functions: "))
+    
+    # Initial basis and knot vector
+    basis, knot_vector = create_basis(degree=degree, num_basis_functions=num_basis_functions)
+    x_vals, evals = compute_basis_evals(basis)
+
+    # Plot the initial basis functions
+    lines = []
+    for i in range(basis.size()):
+        line, = ax.plot(x_vals, evals[i, :], label=f'Basis function {i + 1}')
+        lines.append(line)
+
+    # Initial dotted lines for inner knots
+    inner_knot_lines = ax.vlines(
+        knot_vector[degree + 1 : -(degree + 1)],
+        ymin=0, ymax=1, colors='gray', linestyles='dotted', label='Knots'
+    )
+
+    ax.set_title('B-spline basis functions')
+    ax.set_xlabel(r'$\xi$')
+    ax.set_ylabel(r'$\varphi$')
+
+    # Add sliders for custom knots
+    custom_knot_axes = []
+    custom_knot_sliders = []
+    inner_knots = len(knot_vector) - 2 * (degree + 1)
+    for i in range(inner_knots):
+        ax_slider = plt.axes([0.25, 0.25 - i * 0.05, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+        slider = Slider(ax_slider, f'Knot {i + 1}', 0.01, 0.99, valinit=knot_vector[degree + 1 + i])
+        custom_knot_sliders.append(slider)
+        custom_knot_axes.append(ax_slider)
+    
+    def update():
+        # Collect current slider values for the inner knots
+        custom_knots = np.array([slider.val for slider in custom_knot_sliders])
+
+        # Generate new basis and knot vector
+        basis, updated_knot_vector = create_basis(degree, num_basis_functions, custom_knots=custom_knots)
+        x_vals, evals = compute_basis_evals(basis)
+
+        # Update basis function plots
+        for i, line in enumerate(lines):
+            line.set_data(x_vals, evals[i, :])
+
+        # Update inner knot lines
+        # Remove existing inner knot lines
+        for collection in ax.collections:
+            collection.remove()
+        ax.vlines(
+            updated_knot_vector[degree + 1 : -(degree + 1)],
+            ymin=0, ymax=1, colors='gray', linestyles='dotted', label='Knots'
+        )
+
+        # Refresh the figure
+        fig.canvas.draw_idle()
+
+    # Connect sliders to the update function
+    for slider in custom_knot_sliders:
+        slider.on_changed(lambda val, slider=slider: update())
+    
+    plt.show()
+
+
+
                              
 if __name__ == "__main__":
-    plot()
+    plot_with_knot_sliders()
+    #plot_degree_functions_sliders()
